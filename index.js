8b5059f744d6fd02f0f9dae319a2f65df5379021
nyawiradev <njaginjoyce@gmail.com>
2022-06-08 03:35:44 +0300

    commit tree 10e17e046d5800f85142204b3e9d8d787773aa02

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'Bob';
+
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+   // var bestCustomer = '';
+   return bestCustomer = 'not Bob';
+}
+function overwriteBestCustomer() {
+   return bestCustomer = 'maybe moses';
+}
+const leastFavoriteCustomer = 'Abdo';
+function changeLeastFavoriteCustomer() {
+   return leastFavoriteCustomer = 'CHANGE';
+}
